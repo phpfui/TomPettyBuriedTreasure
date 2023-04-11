359e67e5676a5ccce1510f043084bba101152a20
Bruce Wells <brucekwells@gmail.com>
2023-04-11 09:18:38 -0400

    Better deploy script

--- www/update.php
+++ www/update.php
@@ -12,7 +12,10 @@ include '../common.php';
 \ini_set('display_startup_errors', 1);
 
 $repo = new \Gitonomy\Git\Repository($_SERVER['DOCUMENT_ROOT'] . '/..');
+$repo->run('prune');         // remove old versions and branches
+$repo->run('stash');         // stash any changed files, user can get back stash if needed so we won't delete it
+$repo->run('clean', ['-f']); // remove unstaged files
 $wc = $repo->getWorkingCopy();
-$wc->checkout('master');
+$wc->checkout('main');
 $repo->run('pull');
 \header('location: /');
